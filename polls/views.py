d475478cfe531d7ba8b7f343c6a58f1f19feba3a
Pawan <pawanghising67@gmail.com>
2019-07-25 22:17:46 -0500

    Initial Commit

---
+++ polls/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question,Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice =  question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html',{
+            'question': question,
+            'error_message': "You din't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results',
+                                            args=(question.id,)))
